d97d44ac553efa70f5b6434b1f9860bd86559a84
Anson Ong <anson.ong@mail.utoronto.ca>
2025-10-17 20:04:51 -0400

    Complete Task 4: Convert BreedNotFoundException to checked exception and update method signatures

--- src/main/java/dogapi/BreedFetcher.java
+++ src/main/java/dogapi/BreedFetcher.java
@@ -13,12 +13,11 @@ public interface BreedFetcher {
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist
      */
-    List<String> getSubBreeds(String breed);
+    List<String> getSubBreeds(String breed) throws BreedNotFoundException;
 
 
-    // TODO Task 4: make this a checked exception and update any other code as needed.
     // a class defined in an interface is public AND static
-    class BreedNotFoundException extends RuntimeException {
+    class BreedNotFoundException extends Exception {
         public BreedNotFoundException(String breed) {
             super("Breed not found: " + breed);
         }
