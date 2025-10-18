d3a5a0d29cbc59ef46e2fdf77a8f4ca663ad08a2
Anson Ong <anson.ong@mail.utoronto.ca>
2025-10-17 20:04:42 -0400

    Complete Tasks 1-3: Implement DogApiBreedFetcher, CachingBreedFetcher, and Main methods

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -24,8 +24,11 @@ public class Main {
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
-        // return statement included so that the starter code can compile and run.
-        return -1;
+        try {
+            List<String> subBreeds = breedFetcher.getSubBreeds(breed);
+            return subBreeds.size();
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            return 0;
+        }
     }
 }
